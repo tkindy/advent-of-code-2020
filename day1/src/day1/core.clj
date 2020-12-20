b298d1a195c3657a940c24eaee1057b86f443c1f
Tyler Kindy <tylerkindy@gmail.com>
2020-12-19 22:32:01 -0500

    Day 1 part 1

--- day1/src/day1/core.clj
+++ day1/src/day1/core.clj
@@ -1,7 +1,19 @@
 (ns day1.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.string :as str]))
+
+(defn cart [colls]
+  (if (empty? colls)
+    '(())
+    (for [more (cart (rest colls))
+          x (first colls)]
+      (cons x more))))
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (let [numbers (->> (str/split (slurp "resources/input") #"\n")
+                     (map #(Integer/parseInt %)))
+        pairs (cart (list numbers numbers))
+        match (first (filter (fn [pair] (= (reduce + pair) 2020)) pairs))
+        product (* (first match) (second match))]
+    (println product)))
--- day1/test/day1/core_test.clj
+++ day1/test/day1/core_test.clj
@@ -1,7 +1,8 @@
 (ns day1.core-test
   (:require [clojure.test :refer :all]
-            [day1.core :refer :all]))
+            [day1.core :refer (cart)]))
 
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (testing "cart"
+    (is (= (set (cart '((a b) (1 2 3))))
+           (set '((a 1) (a 2) (a 3) (b 1) (b 2) (b 3)))))))
