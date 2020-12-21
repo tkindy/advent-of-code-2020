18ebb7364c1c1b4d3fa6db472ace08c1f8bafa04
Tyler Kindy <tylerkindy@gmail.com>
2020-12-21 09:02:05 -0500

    Add tests

--- day4/test/day4/core_test.clj
+++ day4/test/day4/core_test.clj
@@ -1,6 +1,6 @@
 (ns day4.core-test
   (:require [clojure.test :refer :all]
-            [day4.core :refer [split-passports parse-passport]]))
+            [day4.core :refer [split-passports parse-passport valid?]]))
 
 (def example-input
   "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
@@ -16,6 +16,14 @@ hgt:179cm
 
 hcl:#cfa07d eyr:2025 pid:166559648
 iyr:2011 ecl:brn hgt:59in")
+(def all-fields {:ecl "gry" :pid "860033327" :eyr "2020" :hcl "#fffffd"
+                 :byr "1937" :iyr "2017" :cid "147" :hgt "183cm"})
+(def missing-hgt {:iyr "2013" :ecl "amb" :cid "350" :eyr "2023"
+                  :pid "028048884" :hcl "#cfa07d" :byr "1929"})
+(def missing-cid {:hcl "#ae17e1" :iyr "2013" :eyr "2024" :ecl "brn"
+                  :pid "760753108" :byr "1931" :hgt "179cm"})
+(def missing-cid-byr {:hcl "#cfa07d" :eyr "2025" :pid "166559648"
+                      :iyr "2011" :ecl "brn" :hgt "59in"})
 
 (deftest test-split-passports
   (is (= (split-passports example-input)
@@ -28,3 +36,9 @@ iyr:2011 ecl:brn hgt:59in")
   (is (= (parse-passport "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd byr:1937 iyr:2017 cid:147 hgt:183cm")
          {:ecl "gry" :pid "860033327" :eyr "2020" :hcl "#fffffd"
           :byr "1937" :iyr "2017" :cid "147" :hgt "183cm"})))
+
+(deftest test-valid?
+  (is (valid? all-fields))
+  (is (not (valid? missing-hgt)))
+  (is (valid? missing-cid))
+  (is (not (valid? missing-cid-byr))))
