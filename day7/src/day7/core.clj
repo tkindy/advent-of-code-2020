2732dd823e7727ca08697e13b15cb99bbf8f1e44
Tyler Kindy <tylerkindy@gmail.com>
2020-12-21 18:26:10 -0500

    Implement rule parsing

--- day7/src/day7/core.clj
+++ day7/src/day7/core.clj
@@ -1,5 +1,35 @@
 (ns day7.core
-    (:gen-class))
+  (:gen-class)
+  (:require [clojure.string :as str]))
+
+(def rule-pattern #"([a-z ]+) bags contain (.*)\.")
+(def content-pattern #"(\d+) ([a-z ]+) bags?")
+
+(defn parse-content
+  [content]
+  (let [[_ count color] (re-matches content-pattern content)]
+    {:color color :count (Integer/parseInt count)}))
+
+(defn parse-contents
+  [contents]
+  (if (= contents "no other bags")
+    []
+    (let [splits (str/split contents #", ")]
+      (map parse-content splits))))
+
+(defn parse-rule
+  [line]
+  (let [[_ color contents] (re-matches rule-pattern line)
+        contents (parse-contents contents)]
+    {:color color :contents contents}))
+
+(defn parse-input
+  [input]
+  (map parse-rule (str/split-lines input)))
+
+(defn read-input
+  []
+  (parse-input (slurp "resources/input")))
 
 (defn -main
   [& args]
--- day7/test/day7/core_test.clj
+++ day7/test/day7/core_test.clj
@@ -1,7 +1,38 @@
 (ns day7.core-test
-    (:require [clojure.test :refer :all]
-              [day7.core :refer :all]))
+  (:require [clojure.test :refer :all]
+            [day7.core :refer [parse-input parse-rule]]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def example-rules
+  "light red bags contain 1 bright white bag, 2 muted yellow bags.
+dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+bright white bags contain 1 shiny gold bag.
+muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+faded blue bags contain no other bags.
+dotted black bags contain no other bags.")
+(def example-parsed
+  [{:color "light red" :contents [{:color "bright white" :count 1}
+                                  {:color "muted yellow" :count 2}]}
+   {:color "dark orange" :contents [{:color "bright white" :count 3}
+                                    {:color "muted yellow" :count 4}]}
+   {:color "bright white" :contents [{:color "shiny gold" :count 1}]}
+   {:color "muted yellow" :contents [{:color "shiny gold" :count 2}
+                                     {:color "faded blue" :count 9}]}
+   {:color "shiny gold" :contents [{:color "dark olive" :count 1}
+                                   {:color "vibrant plum" :count 2}]}
+   {:color "dark olive" :contents [{:color "faded blue" :count 3}
+                                   {:color "dotted black" :count 4}]}
+   {:color "vibrant plum" :contents [{:color "faded blue" :count 5}
+                                     {:color "dotted black" :count 6}]}
+   {:color "faded blue" :contents []}
+   {:color "dotted black" :contents []}])
+
+(deftest test-parse-rule
+  (is (= (parse-rule "light red bags contain 1 bright white bag, 2 muted yellow bags.")
+         {:color "light red" :contents [{:color "bright white" :count 1}
+                                        {:color "muted yellow" :count 2}]})))
+
+(deftest test-parse-input
+  (is (= (parse-input example-rules) example-parsed)))
