8c3146ba4c0001d97ee03cd035679d1fa62d5125
Tyler Kindy <tylerkindy@gmail.com>
2020-12-21 15:20:56 -0500

    Implement part 1

--- day6/src/day6/core.clj
+++ day6/src/day6/core.clj
@@ -1,6 +1,30 @@
 (ns day6.core
-    (:gen-class))
+  (:gen-class)
+  (:require [clojure.string :as str]
+            [clojure.set :as set]))
+
+(defn parse-person
+  [person]
+  (set person))
+
+(defn parse-group
+  [group]
+  (map parse-person (str/split group #"\n")))
+
+(defn parse-input
+  [input]
+  (map parse-group (str/split input #"\n\n")))
+
+(defn read-input
+  []
+  (parse-input (slurp "resources/input")))
+
+(defn any-yes
+  [group]
+  (apply set/union group))
 
 (defn -main
   [& args]
-  (println "Hello, World!"))
+  (let [groups (read-input)
+        yes-counts (map (comp count any-yes) groups)]
+    (println "Part 1:" (reduce + 0 yes-counts))))
