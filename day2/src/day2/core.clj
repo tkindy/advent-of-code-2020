3106d990020d31be7263860e72ccaf2ff0292173
Tyler Kindy <tylerkindy@gmail.com>
2020-12-21 06:56:29 -0500

    Add line parsing

--- day2/src/day2/core.clj
+++ day2/src/day2/core.clj
@@ -1,5 +1,23 @@
 (ns day2.core
-    (:gen-class))
+  (:gen-class))
+
+(defn parse-line
+  [line]
+  (let [[_ min-count max-count letter password] (re-find #"^(\d+)-(\d+) (\w): (\w+)$" line)]
+    {:min-count (Integer/parseInt min-count)
+     :max-count (Integer/parseInt max-count)
+     :letter (.charAt letter 0) :password password}))
+
+(defn read-input
+  []
+  (with-open [rdr (clojure.java.io/reader "resources/input")]
+    (map parse-line rdr)))
+
+(defn count-occurrences
+  [s c]
+  (->> s
+       (filter (fn [l] (= l c)))
+       count))
 
 (defn -main
   [& args]
--- day2/test/day2/core_test.clj
+++ day2/test/day2/core_test.clj
@@ -1,7 +1,12 @@
 (ns day2.core-test
-    (:require [clojure.test :refer :all]
-              [day2.core :refer :all]))
+  (:require [clojure.test :refer :all]
+            [day2.core :refer [parse-line count-occurrences]]))
 
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (testing "parse-line"
+    (is (= (parse-line "1-3 a: abcde")
+           {:min-count 1 :max-count 3
+            :letter \a :password "abcde"})))
+  (testing "count-occurrences"
+    (is (= (count-occurrences "abcdbab" \b)
+           3))))
