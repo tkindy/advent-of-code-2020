e32238b2cbb4999a0d6f6f223ae4c91b7672e917
Tyler Kindy <tylerkindy@gmail.com>
2020-12-21 08:47:45 -0500

    Implement input parsing

--- day4/src/day4/core.clj
+++ day4/src/day4/core.clj
@@ -1,5 +1,25 @@
 (ns day4.core
-    (:gen-class))
+  (:gen-class)
+  (:require [clojure.string :as s]))
+
+(defn split-passports
+  [input]
+  (let [passport-lines (s/split input #"\n\n")]
+    (map #(s/replace % #"\n" " ") passport-lines)))
+
+(defn parse-passport
+  [line]
+  (let [pairs (s/split line #"\s+")]
+    (reduce (fn [acc pair]
+              (let [[k v] (s/split pair #":")]
+                (assoc acc (keyword k) v)))
+            {} pairs)))
+
+(defn read-input
+  []
+  (->> (slurp "resources/input")
+       split-passports
+       (map parse-passport)))
 
 (defn -main
   [& args]
--- day4/test/day4/core_test.clj
+++ day4/test/day4/core_test.clj
@@ -1,7 +1,30 @@
 (ns day4.core-test
-    (:require [clojure.test :refer :all]
-              [day4.core :refer :all]))
+  (:require [clojure.test :refer :all]
+            [day4.core :refer [split-passports parse-passport]]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def example-input
+  "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+byr:1937 iyr:2017 cid:147 hgt:183cm
+
+iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
+hcl:#cfa07d byr:1929
+
+hcl:#ae17e1 iyr:2013
+eyr:2024
+ecl:brn pid:760753108 byr:1931
+hgt:179cm
+
+hcl:#cfa07d eyr:2025 pid:166559648
+iyr:2011 ecl:brn hgt:59in")
+
+(deftest test-split-passports
+  (is (= (split-passports example-input)
+         ["ecl:gry pid:860033327 eyr:2020 hcl:#fffffd byr:1937 iyr:2017 cid:147 hgt:183cm"
+          "iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884 hcl:#cfa07d byr:1929"
+          "hcl:#ae17e1 iyr:2013 eyr:2024 ecl:brn pid:760753108 byr:1931 hgt:179cm"
+          "hcl:#cfa07d eyr:2025 pid:166559648 iyr:2011 ecl:brn hgt:59in"])))
+
+(deftest test-parse-passport
+  (is (= (parse-passport "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd byr:1937 iyr:2017 cid:147 hgt:183cm")
+         {:ecl "gry" :pid "860033327" :eyr "2020" :hcl "#fffffd"
+          :byr "1937" :iyr "2017" :cid "147" :hgt "183cm"})))
