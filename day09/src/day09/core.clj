2b804a0ab4ff443e4b44ab53f433f169b9a2bc4f
Tyler Kindy <tylerkindy@gmail.com>
2020-12-22 18:29:30 -0500

    Implement part 2

--- day09/src/day09/core.clj
+++ day09/src/day09/core.clj
@@ -33,7 +33,26 @@
                     (subvec nums preamble-length))]
     (first (first (filter #(not (second %)) with-valid)))))
 
+(defn check-window
+  [nums invalid window-size]
+  (loop [index 0]
+    (if (> (+ index window-size) (count nums))
+      nil
+      (let [window (subvec nums index (+ index window-size))]
+        (if (= (apply + window) invalid)
+          window
+          (recur (+ index 1)))))))
+
+(defn find-weakness
+  [nums invalid]
+  (let [window (some #(check-window nums invalid %)
+                     (range 2 (+ (count nums) 1)))]
+    (+ (apply min window)
+       (apply max window))))
+
 (defn -main
   [& args]
-  (let [nums (read-input)]
-    (println "Part 1:" (first-invalid nums))))
+  (let [nums (read-input)
+        invalid (first-invalid nums)]
+    (println "Part 1:" invalid)
+    (println "Part 2:" (find-weakness nums invalid))))
