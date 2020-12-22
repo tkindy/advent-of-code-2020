1c60cdd99a729f164cadd18283af00e35601bc62
Tyler Kindy <tylerkindy@gmail.com>
2020-12-22 18:57:23 -0500

    Implement part 1

--- day10/src/day10/core.clj
+++ day10/src/day10/core.clj
@@ -1,6 +1,38 @@
 (ns day10.core
-    (:gen-class))
+  (:gen-class)
+  (:require [clojure.string :as str]))
+
+(defn parse-input
+  [input]
+  (->> input
+       str/split-lines
+       (map #(Integer/parseInt %))
+       (reduce conj [])))
+
+(defn read-input
+  []
+  (parse-input (slurp "resources/input")))
+
+(defn make-pairs
+  [l]
+  (first
+   (reduce (fn [[acc last] x]
+             (let [acc (if last
+                         (conj acc [last x])
+                         acc)]
+               [acc x]))
+           [[] nil]
+           l)))
+
+(defn find-joltage-dist
+  [adapters]
+  (let [adapters (sort (conj adapters 0 (+ 3 (apply max adapters))))
+        pairs (make-pairs adapters)
+        differences (map (fn [[a b]] (- b a)) pairs)]
+    (* (count (filter #(= 1 %) differences))
+       (count (filter #(= 3 %) differences)))))
 
 (defn -main
   [& args]
-  (println "Hello, World!"))
+  (let [adapters (read-input)]
+    (println "Part 1:" (find-joltage-dist adapters))))
