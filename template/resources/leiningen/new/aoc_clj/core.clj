361d2f5bd3be100da27708e04767ce3d10c65d63
Tyler Kindy <tylerkindy@gmail.com>
2020-12-22 18:37:10 -0500

    Add input reading skeletons to template

--- template/resources/leiningen/new/aoc_clj/core.clj
+++ template/resources/leiningen/new/aoc_clj/core.clj
@@ -1,6 +1,13 @@
 (ns {{name}}.core
     (:gen-class))
 
+(defn parse-input
+  [input])
+
+(defn read-input
+  []
+  (parse-input (slurp "resources/input")))
+
 (defn -main
   [& args]
   (println "Hello, World!"))
