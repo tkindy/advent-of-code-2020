e1c9c84d84cbdbd67fed1d6bfa9fe12c8ba6f7c7
Tyler Kindy <tylerkindy@gmail.com>
2020-12-22 10:51:04 -0500

    Add data definition

--- day09/src/day09/core.clj
+++ day09/src/day09/core.clj
@@ -2,6 +2,18 @@
   (:gen-class)
   (:require [clojure.string :as str]))
 
+; A Cube is a Boolean indicating whether the cube
+; is active or inactive
+
+; A Line is a SortedMap<Integer, Cube>
+; wher ethe keys are x-coordinates
+
+; A Plane is a SortedMap<Integer, Line>
+; where the keys are y-coordinates
+
+; A Space is a SortedMap<Integer, Plane>
+; where the keys are z-coordinates
+
 (defn parse-line
   [line]
   (->> line
