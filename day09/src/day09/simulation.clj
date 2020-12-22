0ea6e99b1d8bfb867dc90c6b11e02419b5c28726
Tyler Kindy <tylerkindy@gmail.com>
2020-12-22 15:20:06 -0500

    Implemnt cycling a cube

--- day09/src/day09/simulation.clj
+++ day09/src/day09/simulation.clj
@@ -1,4 +1,19 @@
-(ns day09.simulation)
+(ns day09.simulation
+  (:require [clojure.core.match :refer [match]]))
+
+(defn count-active
+  [cubes]
+  (->> cubes
+       (filter (fn [cube] cube))
+       count))
+
+(defn run-cycle-cube
+  [cube neighbors]
+  (let [num-active-neighbors (count-active neighbors)]
+    (match [cube num-active-neighbors]
+      [true (:or 2 3)] true
+      [false 3] true
+      :else false)))
 
 (defn run-cycle
   [space]
