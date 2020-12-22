1ea42c3e7ff464da18f839a486cdf4642d6b120a
Tyler Kindy <tylerkindy@gmail.com>
2020-12-22 18:17:35 -0500

    Implement part 1

--- day09/src/day09/core.clj
+++ day09/src/day09/core.clj
@@ -1,6 +1,39 @@
 (ns day09.core
-    (:gen-class))
+  (:gen-class)
+  (:require [clojure.string :as str]
+            [clojure.math.combinatorics :as combo]))
+
+(defn parse-input
+  [input]
+  (->> input
+       str/split-lines
+       (map #(Long/parseLong %))
+       (reduce conj [])))
+
+(defn read-input
+  []
+  (parse-input (slurp "resources/input")))
+
+(defn valid?
+  [n summand-pool]
+  (let [candidates (->> (combo/cartesian-product summand-pool summand-pool)
+                        (filter (fn [[a b]] (not= a b))))]
+    (some (fn [[a b]] (= n (+ a b))) candidates)))
+
+(def preamble-length 25)
+
+(defn first-invalid
+  [nums]
+  (let [with-valid (map-indexed
+                    (fn [index n]
+                      (let [index (+ index preamble-length)]
+                        [n
+                         (valid? n
+                                 (subvec nums (- index preamble-length) index))]))
+                    (subvec nums preamble-length))]
+    (first (first (filter #(not (second %)) with-valid)))))
 
 (defn -main
   [& args]
-  (println "Hello, World!"))
+  (let [nums (read-input)]
+    (println "Part 1:" (first-invalid nums))))
