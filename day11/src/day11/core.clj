6d94667c283d44349f9bfdbd0c59c7daa3dd483e
Tyler Kindy <tylerkindy@gmail.com>
2020-12-23 08:18:12 -0500

    Add day 11 skeleton

---
+++ day11/src/day11/core.clj
@@ -0,0 +1,13 @@
+(ns day11.core
+    (:gen-class))
+
+(defn parse-input
+  [input])
+
+(defn read-input
+  []
+  (parse-input (slurp "resources/input")))
+
+(defn -main
+  [& args]
+  (println "Hello, World!"))
