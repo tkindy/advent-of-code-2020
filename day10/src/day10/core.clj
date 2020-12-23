692973660e415871875dfdb375cb0112bfbb0f47
Tyler Kindy <tylerkindy@gmail.com>
2020-12-22 22:34:17 -0500

    Implement slow solution

--- day10/src/day10/core.clj
+++ day10/src/day10/core.clj
@@ -1,6 +1,7 @@
 (ns day10.core
   (:gen-class)
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.math.combinatorics :as combo]))
 
 (defn parse-input
   [input]
@@ -24,15 +25,45 @@
            [[] nil]
            l)))
 
-(defn find-joltage-dist
+(defn get-joltages
   [adapters]
-  (let [adapters (sort (conj adapters 0 (+ 3 (apply max adapters))))
-        pairs (make-pairs adapters)
+  (conj adapters
+        0 ; outlet
+        (+ 3 (apply max adapters)))) ; phone
+
+(defn find-joltage-dist
+  [joltages]
+  (let [pairs (make-pairs joltages)
         differences (map (fn [[a b]] (- b a)) pairs)]
     (* (count (filter #(= 1 %) differences))
        (count (filter #(= 3 %) differences)))))
 
+(defn build-joltage-graph
+  [joltages]
+  (->> (combo/cartesian-product joltages joltages)
+       (filter (fn [[out in]] (contains? #{1 2 3} (- in out))))
+       (group-by first)
+       (map (fn [[key entries]] [key (into [] (map second entries))]))
+       (apply conj {})))
+
+(defn count-paths
+  [graph from to]
+  (loop [frontier [from] finish-count 0]
+    (if (empty? frontier)
+      finish-count
+      (let [node (first frontier)]
+        (if (= node to)
+          (recur (rest frontier) (+ finish-count 1))
+          (recur (apply conj
+                        (rest frontier)
+                        (get graph node))
+                 finish-count))))))
+
 (defn -main
   [& args]
-  (let [adapters (read-input)]
-    (println "Part 1:" (find-joltage-dist adapters))))
+  (let [adapters (read-input)
+        joltages (sort (get-joltages adapters))]
+    (println "Part 1:" (find-joltage-dist joltages))
+    (println "Part 2:" (count-paths (build-joltage-graph joltages)
+                                    0
+                                    (apply max joltages)))))
--- day10/test/day10/core_test.clj
+++ day10/test/day10/core_test.clj
@@ -1,7 +1,21 @@
 (ns day10.core-test
-    (:require [clojure.test :refer :all]
-              [day10.core :refer :all]))
+  (:require [clojure.test :refer :all]
+            [day10.core :refer [build-joltage-graph]]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def joltages [16 10 15 5 1 11
+               7 19 6 12 4 0 22])
+
+(deftest test-build-joltage-graph
+  (is (= (build-joltage-graph joltages)
+         {16 [19]
+          10 [11 12]
+          15 [16]
+          5  [7 6]
+          1  [4]
+          11 [12]
+          7  [10]
+          19 [22]
+          6  [7]
+          12 [15]
+          4  [5 7 6]
+          0  [1]})))
