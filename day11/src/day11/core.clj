c8963963ac3cf29bdbe27d960fc1fd1a95355a9d
Tyler Kindy <tylerkindy@gmail.com>
2020-12-23 08:25:29 -0500

    Implement input parsing

--- day11/src/day11/core.clj
+++ day11/src/day11/core.clj
@@ -1,8 +1,27 @@
 (ns day11.core
-    (:gen-class))
+  (:gen-class)
+  (:require [clojure.string :as str]
+            [clojure.core.match :refer [match]]))
+
+(defn parse-seat
+  [seat]
+  (match seat
+    \. 'floor
+    \L 'empty
+    \# 'occupied))
+
+(defn parse-row
+  [row]
+  (->> row
+       (map-indexed (fn [index seat] [index (parse-seat seat)]))
+       (apply conj {})))
 
 (defn parse-input
-  [input])
+  [input]
+  (->> input
+       str/split-lines
+       (map-indexed (fn [index row] [index (parse-row row)]))
+       (apply conj {})))
 
 (defn read-input
   []
