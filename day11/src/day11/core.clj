1ddf4cca2f5e77666ccce1f0c4d34c17ccd6c1a2
Tyler Kindy <tylerkindy@gmail.com>
2020-12-23 11:08:06 -0500

    Implement part 1

--- day11/src/day11/core.clj
+++ day11/src/day11/core.clj
@@ -1,7 +1,8 @@
 (ns day11.core
   (:gen-class)
   (:require [clojure.string :as str]
-            [clojure.core.match :refer [match]]))
+            [clojure.core.match :refer [match]]
+            [clojure.math.combinatorics :as combo]))
 
 (defn parse-seat
   [seat]
@@ -27,6 +28,93 @@
   []
   (parse-input (slurp "resources/input")))
 
+(defn get-seat
+  [{:keys [x y]} seat-map]
+  (get-in seat-map [y x]))
+
+(defn filter-to-range
+  [nums max]
+  (filter #(<= 0 % (- max 1)) nums))
+
+(defn get-neighbor-locs
+  [loc width height]
+  (let [{:keys [x y]} loc]
+    (->> (combo/cartesian-product (filter-to-range [(- y 1) y (+ y 1)] height)
+                                  (filter-to-range [(- x 1) x (+ x 1)] width))
+         (map (fn [[y x]] {:x x :y y}))
+         (filter #(not= % loc)))))
+
+(defn get-neighbors
+  [loc seat-map]
+  (map #(get-seat % seat-map)
+       (get-neighbor-locs loc
+                          (count (get seat-map 0))
+                          (count seat-map))))
+
+(defn count-occupied
+  [seats]
+  (->> seats
+       (filter #(= % 'occupied))
+       count))
+
+(defn simulate-one-spot
+  [loc seat-map]
+  (let [seat (get-seat loc seat-map)
+        neighbors (get-neighbors loc seat-map)]
+    (match [seat (count-occupied neighbors)]
+      ['empty 0] 'occupied
+      ['occupied (true :<< #(>= % 4))] 'empty
+      :else seat)))
+
+(defn get-all-spots
+  [seat-map]
+  (->> (combo/cartesian-product (range (count (get seat-map 0)))
+                                (range (count seat-map)))
+       (map (fn [[x y]] {:x x :y y}))))
+
+(defn simulate-one
+  [seat-map]
+  (reduce (fn [new-map loc]
+            (let [{:keys [x y]} loc]
+              (assoc-in new-map [y x] (simulate-one-spot loc seat-map))))
+          {}
+          (get-all-spots seat-map)))
+
+(defn simulate
+  [seat-map]
+  (loop [cur-map seat-map, last-map nil]
+    (if (= cur-map last-map)
+      cur-map
+      (recur (simulate-one cur-map) cur-map))))
+
+(defn count-occupied-map
+  [seat-map]
+  (->> seat-map
+       vals
+       (map #(count-occupied (vals %)))
+       (apply +)))
+
+(defn sort-map
+  [m]
+  (apply sorted-map (flatten (seq m))))
+
+(defn spot->string
+  [spot]
+  (match spot
+    'floor "."
+    'empty "L"
+    'occupied "#"))
+
+(defn row->string
+  [row]
+  (str/join (map spot->string (vals (sort-map row)))))
+
+(defn seat-map->string
+  [seat-map]
+  (str/join "\n"
+            (map row->string (vals (sort-map seat-map)))))
+
 (defn -main
   [& args]
-  (println "Hello, World!"))
+  (let [seat-map (read-input)]
+    (println "Part 1:" (count-occupied-map (simulate seat-map)))))
--- day11/test/day11/core_test.clj
+++ day11/test/day11/core_test.clj
@@ -1,7 +1,45 @@
 (ns day11.core-test
-    (:require [clojure.test :refer :all]
-              [day11.core :refer :all]))
+  (:require [clojure.test :refer :all]
+            [clojure.string :as str]
+            [day11.core :as c]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defn read-example
+  [name]
+  (str/trim (slurp (str "resources/" name))))
+
+(def example-string (read-example "example"))
+(def example (c/parse-input example-string))
+
+(deftest seat-map->string
+  (is (= (c/seat-map->string example)
+         example-string)))
+
+(deftest simulate-one-spot
+  (is (= (c/simulate-one-spot {:x 5 :y 1}
+                              (c/parse-input (read-example "example-simulated1")))
+         'empty)))
+
+(deftest get-neighbors
+  (is (= (c/get-neighbors {:x 8 :y 4} (c/parse-input (read-example "example-simulated2")))
+         ['floor 'empty    'occupied
+          'floor           'empty
+          'floor 'occupied 'occupied])
+      (= (c/get-neighbors {:x 0 :y 0} (c/parse-input (read-example "example-simulated2")))
+         ['floor 'occupied 'empty])))
+
+(deftest get-seat
+  (is (= (c/get-seat {:x 0 :y 0} example) 'empty))
+  (is (= (c/get-seat {:x 3 :y 2} example) 'floor))
+  (is (= (c/get-seat {:x 6 :y 5} (c/parse-input (read-example "example-simulated2"))) 'occupied)))
+
+(deftest get-neighbor-locs
+  (is (= (c/get-neighbor-locs {:x 4 :y 8} 12 15)
+         [{:x 3 :y 7} {:x 4 :y 7} {:x 5 :y 7}
+          {:x 3 :y 8}             {:x 5 :y 8}
+          {:x 3 :y 9} {:x 4 :y 9} {:x 5 :y 9}])))
+
+(deftest simulate-one
+  (is (= (c/seat-map->string (c/simulate-one example))
+         (read-example "example-simulated1")))
+  (is (= (c/seat-map->string (c/simulate-one (c/simulate-one example)))
+         (read-example "example-simulated2"))))
