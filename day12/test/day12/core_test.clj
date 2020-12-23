ce457bfa799a20c83391b3034934b2b02e817701
Tyler Kindy <tylerkindy@gmail.com>
2020-12-23 13:35:01 -0500

    Add parsing tests

--- day12/test/day12/core_test.clj
+++ day12/test/day12/core_test.clj
@@ -1,7 +1,18 @@
 (ns day12.core-test
-    (:require [clojure.test :refer :all]
-              [day12.core :refer :all]))
+  (:require [clojure.test :refer :all]
+            [day12.core :as c]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def example-string
+  "F10
+N3
+F7
+R90
+F11")
+
+(deftest parse-input
+  (is (= (c/parse-input example-string)
+         [{:action "F" :value 10}
+          {:action "N" :value 3}
+          {:action "F" :value 7}
+          {:action "R" :value 90}
+          {:action "F" :value 11}])))
